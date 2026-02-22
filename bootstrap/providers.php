e73035fc11bc0d0750b80a28fe82ac30a2e8f2b0
alisufyanali <alisufyan2410@gmail.com>
2026-02-22 14:50:56 +0500

    telescopes install

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -5,4 +5,5 @@ return [
     App\Providers\AuthServiceProvider::class,
     App\Providers\BroadcastServiceProvider::class,
     App\Providers\FortifyServiceProvider::class,
+    App\Providers\TelescopeServiceProvider::class,
 ];
